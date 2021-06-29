06b37c0fe17577b8238d04004db256196149ba85
DouglasTSamuel <douglas.netf@gmail.com>
2021-06-28 22:52:05 -0300

    alterando organização código JS

--- bootcampHtml&Css/jogoSnake/script.js
+++ bootcampHtml&Css/jogoSnake/script.js
@@ -11,7 +11,6 @@ let food = {
   x: Math.floor(Math.random() * 15 + 1) * box,
   y: Math.floor(Math.random() * 15 + 1) * box,
 };
-
 let jogo = setInterval(iniciarJogo, 100);
 
 function criarBG() {
@@ -24,20 +23,6 @@ function criarCobrinha() {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
-}
-
-document.addEventListener("keydown", update);
-
-function drawFood() {
-  context.fillStyle = "red";
-  context.fillRect(food.x, food.y, box, box);
-}
-
-function update() {
-  if (event.keyCode === 37 && direction !== "right") direction = "left";
-  if (event.keyCode === 38 && direction !== "down") direction = "up";
-  if (event.keyCode === 39 && direction !== "left") direction = "right";
-  if (event.keyCode === 40 && direction !== "up") direction = "down";
 }
 
 function iniciarJogo() {
@@ -77,3 +62,17 @@ function iniciarJogo() {
   };
   snake.unshift(newHead);
 }
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
+
+function update() {
+  if (event.keyCode === 37 && direction !== "right") direction = "left";
+  if (event.keyCode === 38 && direction !== "down") direction = "up";
+  if (event.keyCode === 39 && direction !== "left") direction = "right";
+  if (event.keyCode === 40 && direction !== "up") direction = "down";
+}
+
+document.addEventListener("keydown", update);
